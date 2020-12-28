42486b1e37662d0ce70d9e641d33b651b6bed8a9
billyct <billy_allen@126.com>
2020-12-28 14:05:31 +0800

    test: mock EventTarget for jest

---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  setupFiles: ['./mockEventTarget.js'],
+}
---
+++ mockEventTarget.js
@@ -0,0 +1,44 @@
+/**
+ * jsdom not implement EventTarget yet, see https://github.com/jsdom/jsdom/issues/2156
+ * so create as simple implement for jest, the code is from https://developer.mozilla.org/en-US/docs/Web/API/EventTarget#Example
+ *
+ * @constructor
+ */
+var EventTarget = function() {
+  this.listeners = {};
+};
+
+EventTarget.prototype.listeners = null;
+EventTarget.prototype.addEventListener = function(type, callback) {
+  if (!(type in this.listeners)) {
+    this.listeners[type] = [];
+  }
+  this.listeners[type].push(callback);
+};
+
+EventTarget.prototype.removeEventListener = function(type, callback) {
+  if (!(type in this.listeners)) {
+    return;
+  }
+  var stack = this.listeners[type];
+  for (var i = 0, l = stack.length; i < l; i++) {
+    if (stack[i] === callback){
+      stack.splice(i, 1);
+      return;
+    }
+  }
+};
+
+EventTarget.prototype.dispatchEvent = function(event) {
+  if (!(event.type in this.listeners)) {
+    return true;
+  }
+  var stack = this.listeners[event.type].slice();
+
+  for (var i = 0, l = stack.length; i < l; i++) {
+    stack[i].call(this, event);
+  }
+  return !event.defaultPrevented;
+};
+
+global.EventTarget = EventTarget
