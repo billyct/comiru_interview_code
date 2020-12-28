ac36306a119bd2d5fe16e6bff6f9224a4fd7e432
billyct <billy_allen@126.com>
2020-12-28 21:22:04 +0800

    test: test autocomplete/item

---
+++ src/autocomplete/__test__/item.test.js
@@ -0,0 +1,154 @@
+/* jshint ignore:start */
+const {getByText, fireEvent} = require('@testing-library/dom')
+require('@testing-library/jest-dom/extend-expect')
+
+const Item = require('../item')
+const events = require('../events')
+const classes = require('../classes')
+
+it(`should render the Item component's html`, () => {
+  let item = new Item()
+  expect(item.root).toContainHTML(`<span class="${classes.itemContainer}"></span>`)
+})
+
+describe(`test Item Component's handleSelected method`, () => {
+  it('should call handleSelected method', () => {
+    const mockCallback = jest.fn()
+    const spy = jest.spyOn(Item.prototype, 'handleSelected').mockImplementation(mockCallback)
+
+    const item = new Item()
+
+    item.trigger(events.onSelected)
+
+    expect(mockCallback).toBeCalledTimes(1)
+
+    spy.mockRestore()
+  })
+
+  it(`should render the Item's html`, () => {
+    const item = new Item()
+
+    const selectedValue = 'whatever'
+
+    item.trigger(events.onSelected, selectedValue)
+
+    expect(item.root).toContainHTML(`<span class="${classes.item}"><span class="${classes.itemContent}">${selectedValue}</span><span class="${classes.itemRemove}"></span></span>`)
+  })
+})
+
+describe(`test Item Component's handleClick method`, () => {
+  it('should call handleClick method', () => {
+    const mockCallback = jest.fn()
+    const spy = jest.spyOn(Item.prototype, 'handleClick').mockImplementation(mockCallback)
+
+    const item = new Item()
+
+    fireEvent.click(item.root)
+
+    expect(mockCallback).toBeCalledTimes(1)
+
+    spy.mockRestore()
+  })
+
+  it('should not call target.parentNode.remove()', () => {
+    const item = new Item()
+    item.trigger(events.onSelected, 'whatever')
+
+    const first = item.root.firstElementChild
+
+    fireEvent.click(first.firstElementChild)
+    expect(item.root).toContainElement(first)
+  })
+
+  it('should call target.parentNode.remove()', () => {
+    const item = new Item()
+    item.trigger(events.onSelected, 'whatever')
+
+    const first = item.root.firstElementChild
+
+    fireEvent.click(first.lastElementChild)
+    expect(item.root).not.toContainElement(first)
+  })
+
+  it('should trigger onUnselected event', () => {
+    const mockCallback = jest.fn()
+    const item = new Item()
+
+    item.on(events.onUnselected, mockCallback)
+
+    const selectedValue = 'whatever'
+
+    item.trigger(events.onSelected, selectedValue)
+
+    fireEvent.click(item.root.firstElementChild.lastElementChild)
+
+    expect(mockCallback).toBeCalledTimes(1)
+    expect(mockCallback).toBeCalledWith(new CustomEvent({
+      target: {
+        value: selectedValue,
+      },
+    }))
+  })
+})
+
+describe(`test Item Component's handleUnselected method`, () => {
+  it('should call the handleUnselected method', () => {
+    const mockCallback = jest.fn()
+    const spy = jest.spyOn(Item.prototype, 'handleUnselected').mockImplementation(mockCallback)
+
+    const item = new Item()
+    item.trigger(events.onUnselected)
+
+    expect(mockCallback).toBeCalledTimes(1)
+
+    spy.mockRestore()
+  })
+
+  it('should call node.parentNode.remove()', async () => {
+
+    const item = new Item()
+
+    const selectedValue = 'whatever'
+    item.trigger(events.onSelected, selectedValue)
+
+    const node = await getByText(item.root, selectedValue)
+
+    item.trigger(events.onUnselected, selectedValue)
+    expect(item.root).not.toContainElement(node)
+  })
+
+  it('should not call node.parentNode.remove()', async () => {
+    const item = new Item()
+
+    const selectedValue = 'whatever'
+    item.trigger(events.onSelected, selectedValue)
+
+    const node = await getByText(item.root, selectedValue)
+
+    item.trigger(events.onUnselected, selectedValue + 'x')
+    expect(item.root).toContainElement(node)
+  })
+})
+
+describe(`test Item Component's handlePressBackspace method`, () => {
+  it('should trigger onUnselected event', () => {
+    const mockCallback = jest.fn()
+    const item = new Item()
+    const selectedValue = 'whatever'
+
+    item.on(events.onUnselected, mockCallback)
+
+    item.trigger(events.onSelected, selectedValue)
+
+    item.trigger(events.onPressBackspace)
+
+    expect(mockCallback).toBeCalledTimes(1)
+    expect(mockCallback).toBeCalledWith(new CustomEvent({
+      detail: {
+        value: selectedValue,
+      }
+    }))
+  })
+})
+
+/* jshint ignore:end */
