10cabc1f3f351d7a3e64a6a8436df43e21f01428
billyct <billy_allen@126.com>
2021-01-06 11:32:30 +0800

    test: rewrite highlight.test.js use describe.each

--- src/common/__tests__/highlight.test.js
+++ src/common/__tests__/highlight.test.js
@@ -1,26 +1,14 @@
 /* jshint ignore:start */
 const highlight = require('../highlight')
 
-describe('test highlight function', () => {
-  it('should return the text matched with <strong>', () => {
-    const a = 'abcdefg'
-    const b = 'cde'
 
-    expect(highlight(a, b)).toBe('ab<strong>cde</strong>fg')
-  })
-
-  it('should return the origin text when mismatched', () => {
-    const a = 'abcdefg'
-    const b = 'ce'
-
-    expect(highlight(a, b)).toBe('abcdefg')
-  })
-
-  it('should return the origin text when b is empty', () => {
-    const a = 'abcdefg'
-    const b = ''
-
-    expect(highlight(a, b)).toBe('abcdefg')
+describe.each([
+  ['abcdefg', 'cde', 'ab<strong>cde</strong>fg'],
+  ['abcdefg', 'ce', 'abcdefg'],
+  ['abcdefg', '', 'abcdefg'],
+])(`highlight(%s, %s)`, (a, b, expected) => {
+  it(`should return ${expected}`, () => {
+    expect(highlight(a, b)).toBe(expected)
   })
 })
 /* jshint ignore:end */
